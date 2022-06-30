ad07f1ccb69faf6c57c86a29a63eb7247a7f51b2
Prashant Malusare <prashant_malusare@persistent.com>
2022-06-30 14:21:32 +0530

    React Typescript Assignment

---
+++ src/components/App.tsx
@@ -0,0 +1,38 @@
+import React, { FormEvent } from "react";
+import '../App.css';
+
+async function sendForm(event: FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  const { name, password } = event.target as typeof event.target & {
+    name: { value: string; };
+    password: { value: string; };
+  };
+
+  console.log(name.value, password.value);
+}
+
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <h1>Login Form Using React Typescript</h1>
+
+      <form className="wrap" onSubmit={evt => { sendForm(evt) }}>
+        <fieldset className="field-area">
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" />
+        </fieldset>
+        <fieldset className="field-area">
+          <label htmlFor="password">Pass: </label>
+          <input type="password" id="password" />
+        </fieldset>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
